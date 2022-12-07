9429d12d6d5bcd15fcdd062cb2fba9b59b4961f7
stevo sfalcin <stevo.sfalcin@youxgroup.com.br>
2022-12-07 19:20:14 -0300

    inicia frontend

---
+++ front-end/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-content">
+    <div class="d-flex">
+      <span>teste</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'home-view',
+};
+</script>
+
+<style scoped>
+.home-content {
+  width: 100%;
+}
+
+</style>
